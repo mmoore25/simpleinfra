d7a44530b49574dfe21fc4ab5de6d5b1bd1e8114
mmoore25 <mmoore25@gmail.com>
2021-08-20 08:21:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,8 @@
-
+#############################################################################
+# VARIABLES
+#############################################################################
+variable "tags" {
+  description = "(Optional) A mapping of default tags to assign to the resource"
+  type        = map(string)
+  default     = null
+}
