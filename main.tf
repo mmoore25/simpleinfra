0f184bb13ee0722e424f1115effb2e2553f1533e
mmoore25 <mmoore25@gmail.com>
2021-08-20 10:33:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+#############################################################################
+# PROVIDERS
+#############################################################################
 terraform {
   required_providers {
     azurerm = {
@@ -13,17 +16,17 @@ provider "azurerm" {
 }
 
 
-#
+#############################################################################
 # Resource group creation
-#
+#############################################################################
 resource "azurerm_resource_group" "mainrg" {
   name     = "mainrg"
   location = "East US"
 }
 
-#
+#############################################################################
 # Diagnostic Storage Account
-#
+#############################################################################
 resource "azurerm_storage_account" "diagstorage" {
   name                     = "springpathdiagstor001"
   resource_group_name      = azurerm_resource_group.mainrg.name
@@ -31,14 +34,12 @@ resource "azurerm_storage_account" "diagstorage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-  tags = {
-    environment = "staging"
-  }
+  tags = var.tags
 }
 
-#
+#############################################################################
 # Create the App Service Plan
-#
+#############################################################################
 resource "azurerm_app_service_plan" "main-asp" {
   name                = "plan-springpathnet-live-${azurerm_resource_group.mainrg.location}"
   location            = azurerm_resource_group.mainrg.location
@@ -48,12 +49,14 @@ resource "azurerm_app_service_plan" "main-asp" {
     tier = "Standard"
     size = "S1"
   }
+  
+  tags = var.tags
 }
 
 
-#
+#############################################################################
 # Create the Connect Web App
-#
+#############################################################################
 resource "azurerm_app_service" "connectUI" {
   name                = "app-springpathnet-connect-${azurerm_resource_group.mainrg.location}"
   location            = azurerm_resource_group.mainrg.location
@@ -74,12 +77,14 @@ resource "azurerm_app_service" "connectUI" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
+  
+  tags = var.tags
 } 
 
 
-#
+#############################################################################
 # Create the API Web App
-#
+#############################################################################
 resource "azurerm_app_service" "API" {
   name                = "app-springpathnet-core-api-${azurerm_resource_group.mainrg.location}"
   location            = azurerm_resource_group.mainrg.location
@@ -100,6 +105,8 @@ resource "azurerm_app_service" "API" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
+  
+  tags = var.tags
 } 
 
 resource "azurerm_application_insights" "apiInsights" {
@@ -107,16 +114,19 @@ resource "azurerm_application_insights" "apiInsights" {
   location            = azurerm_resource_group.mainrg.location
   resource_group_name = azurerm_resource_group.mainrg.name
   application_type    = "web"
+  
+  tags = var.tags
 }
 
-#
+#############################################################################
 # Create the Identity Web App
-#
+#############################################################################
 resource "azurerm_app_service" "Identity" {
   name                = "app-springpathnet-identity-${azurerm_resource_group.mainrg.location}"
   location            = azurerm_resource_group.mainrg.location
   resource_group_name = azurerm_resource_group.mainrg.name
   app_service_plan_id = azurerm_app_service_plan.main-asp.id
+  depends_on          = [ azurerm_postgresql_server.dbserver.Id, azurerm_postgresql_database.db.Id  ]
 
   site_config {
     dotnet_framework_version = "v5.0"
@@ -132,12 +142,14 @@ resource "azurerm_app_service" "Identity" {
     type  = "SQLServer"
     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
   }
+  
+  tags = var.tags
 } 
 
 
-#
+#############################################################################
 # add the Postgresql server
-#
+#############################################################################
 resource "azurerm_postgresql_server" "dbserver" {
   name                = "demopgsql2354"
   location            = azurerm_resource_group.mainrg.location
@@ -155,16 +167,20 @@ resource "azurerm_postgresql_server" "dbserver" {
   version                      = "11"
   ssl_enforcement_enabled      = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
+  
+   tags = var.tags
 }
 
 
-#
+#############################################################################
 # Add the database on the Server
-#
+#############################################################################
 resource "azurerm_postgresql_database" "db" {
   name                = "envdb"
   resource_group_name = azurerm_resource_group.mainrg.name
   server_name         = azurerm_postgresql_server.dbserver.name
   charset             = "UTF8"
   collation           = "English_United States.1252"
+  
+  tags = var.tags
 }
